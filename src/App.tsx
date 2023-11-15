4904f451d7a127d9facf349b105454d8bb43e119
Alain PEREZ RIAÑO <alaintxu.p@gmail.com>
2023-11-15 10:57:01 +0000

    FIXED PAGINATION

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,25 @@ import { Filters } from "./components/Filter";
 import { MCCard } from "./components/Card";
 import { CardFilter } from "./components/Filter/Filters";
 import { FilterStatus } from "./components/Filter/MultiselectFilter";
+import { ReactBSPagination } from "@draperez/react-components";
+import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
+import { useTranslation } from "react-i18next";
+
+// Defines the fields where text will be search on text filter
+const textFilterFields = [
+  'name',
+  'real_name',
+  'text',
+  'real_text',
+  'back_text',
+  'flavor',
+  'traits',
+  'real_traits'
+];
 
 
 function App() {
+  const { t } = useTranslation('global');
   const [selectedNavigationItem, setSelectedNavigationItem] = useState<NavigationOptionsKey>("card_list");
 
 
@@ -18,6 +34,47 @@ function App() {
 
   const [filters, setFilters] = useState<CardFilter[]>([]);
   const [filterText, setFilterText] = useState("");
+
+  // Status
+  const [showAllCardData, setShowAllCardData] = useState(false);
+
+  // Filters
+  const evaluateCarFiltering = (filter: CardFilter, card: MCCard): boolean => {
+    const filterValues = filter.filterStatus.selected.map((option) => option.value);
+    const cardValues = card[filter.field] as string[];
+
+
+    const filteredCardValues = filterValues.filter((filterValue) => cardValues.includes(filterValue));
+    if (filter.filterStatus.isAnd)
+      return filteredCardValues.length == filterValues.length;
+    else
+      return filteredCardValues.length > 0;
+  }
+
+  const filteredCards = cards.filter((card) => {
+    for (const filter of filters)
+      if (!evaluateCarFiltering(filter, card))
+        return false
+
+
+    for (const field of textFilterFields)
+      if (String(card[field as keyof MCCard]).toLowerCase().includes(filterText))
+        return true;
+
+    return false;
+  });
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const visibleFirstCardIndex = (currentPage - 1) * cardsPerPage;
+  const visibleLastCardIndex = visibleFirstCardIndex + cardsPerPage;
+  const paginatedCards = filteredCards.slice(
+    visibleFirstCardIndex,
+    visibleLastCardIndex
+  )
+  const totalPages = Math.ceil(filteredCards.length / cardsPerPage);
+
 
   useEffect(() => {
     localStorage.setItem("cards", JSON.stringify(cards));
@@ -58,7 +115,26 @@ function App() {
     <>
       <main id="main-section" className={mainClassNames.join(" ")}>
         <DownloadManager cards={cards} setCards={setCards} />
-        <CardList cards={cards} filters={filters} filterText={filterText} cardsPerPage={cardsPerPage} />
+        <section id="card-list" className='p-3'>
+
+          <button
+            id="show-all-button"
+            className={`btn btn-${showAllCardData ? 'primary' : 'secondary'}`}
+            onClick={() => setShowAllCardData((prev) => !prev)}>
+            {showAllCardData ? <>
+              <BsFillEyeSlashFill title="Esconder datos" />
+            </> : <>
+              <BsFillEyeFill title="Mostrar datos" />
+            </>}
+          </button>
+          <h1>
+            {t('card_list')} &nbsp;
+            <span className='badge text-dark bg-light'>
+              {visibleFirstCardIndex + 1}-{Math.min(...[visibleLastCardIndex, filteredCards.length])}/{filteredCards.length}
+            </span>
+          </h1>
+          <CardList cards={paginatedCards} showAllCardData={showAllCardData} />
+        </section>
         <Filters
           cards={cards}
           filters={filters}
@@ -72,6 +148,13 @@ function App() {
             setFilters([]);
           }} />
       </main>
+      <div className="bg-dark pt-3 shadow">
+      <ReactBSPagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        buttonSize='sm'
+        onPageClick={(pageNumber: number) => setCurrentPage(pageNumber)} />
+      </div>
       <Navigation selected={selectedNavigationItem} active={cards.length > 0} onClick={(item) => setSelectedNavigationItem(item)} />
     </>
   )
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,109 +1,16 @@
 import { Card, MCCard } from './Card';
-import { useState } from 'react';
-import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 
-import { ReactBSPagination } from '@draperez/react-components';
-import { CardFilter } from './Filter/Filters';
-import { useTranslation } from 'react-i18next';
-
-// Defines the fields where text will be search on text filter
-const textFilterFields = [
-  'name',
-  'real_name',
-  'text',
-  'real_text',
-  'back_text',
-  'flavor',
-  'traits',
-  'real_traits'
-];
 
 type Props = {
   cards: MCCard[],
-  filters: CardFilter[],
-  filterText: string,
-  cardsPerPage: number
+  showAllCardData: boolean
 }
-const CardList = ({ cards, filters, filterText, cardsPerPage }: Props) => {
-  const { t } = useTranslation('global');
-
-  // Pagination
-  const [currentPage, setCurrentPage] = useState(1);
-
-  // Status
-  const [showAllCardData, setShowAllCardData] = useState(false);
-
-  // Filters
-  const evaluateCarFiltering = (filter: CardFilter, card: MCCard): boolean => {
-    const filterValues = filter.filterStatus.selected.map((option) => option.value);
-    const cardValues = card[filter.field] as string[];
-
-
-    const filteredCardValues = filterValues.filter((filterValue) => cardValues.includes(filterValue));
-    if (filter.filterStatus.isAnd)
-      return filteredCardValues.length == filterValues.length;
-    else
-      return filteredCardValues.length > 0;
-  }
-
-  const filteredCards = cards.filter((card) => {
-    for (const filter of filters)
-      if (!evaluateCarFiltering(filter, card))
-        return false
-
-
-    for (const field of textFilterFields)
-      if (String(card[field as keyof MCCard]).toLowerCase().includes(filterText))
-        return true;
-
-    return false;
-  });
-
-  const totalPages = Math.ceil(filteredCards.length / cardsPerPage);
-
-  const visibleFirstCardIndex = (currentPage - 1) * cardsPerPage;
-  const visibleLastCardIndex = visibleFirstCardIndex + cardsPerPage;
-  const paginatedCards = filteredCards.slice(
-    visibleFirstCardIndex,
-    visibleLastCardIndex
-  )
+const CardList = ({ cards, showAllCardData }: Props) => {
 
   return (
-    <section id="card-list" className='p-3'>
-      <button
-        id="show-all-button"
-        className={`btn btn-${showAllCardData ? 'primary' : 'secondary'}`}
-        onClick={() => setShowAllCardData((prev) => !prev)}>
-        {showAllCardData ? <>
-          <BsFillEyeSlashFill title="Esconder datos" />
-        </> : <>
-          <BsFillEyeFill title="Mostrar datos" />
-        </>}
-      </button>
-      <h1>
-        {t('card_list')} &nbsp;
-        <span className='badge text-dark bg-light'>
-          {visibleFirstCardIndex + 1}-{Math.min(...[visibleLastCardIndex, filteredCards.length])}/{filteredCards.length}
-        </span>
-      </h1>
-      <div className='p-3'>
-        <ReactBSPagination
-          totalPages={totalPages}
-          currentPage={currentPage}
-          buttonSize='sm'
-          onPageClick={(pageNumber: number) => setCurrentPage(pageNumber)} />
-      </div>
       <div className="card-grid">
-        {paginatedCards.map((card: MCCard) => <Card showCardData={showAllCardData} card={card} key={card.code} />)}
+        {cards.map((card: MCCard) => <Card showCardData={showAllCardData} card={card} key={card.code} />)}
       </div>
-      <div className='p-3'>
-        <ReactBSPagination
-          totalPages={totalPages}
-          currentPage={currentPage}
-          buttonSize='sm'
-          onPageClick={(pageNumber: number) => setCurrentPage(pageNumber)} />
-      </div>
-    </section>
   )
 }
 
