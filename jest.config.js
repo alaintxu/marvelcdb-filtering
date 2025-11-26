5cab181a13a3560857bd119bb25c5bdb39169b4e
Alain PEREZ RIAÑO <alaintxu.p@gmail.com>
2025-11-26 14:14:11 +0100

    chore: update jest configuration to use jsdom environment and add jest-environment-jsdom dependency

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
   setupFiles: ["<rootDir>/src/tests/setupTests.ts"],
-  setupFilesAfterEnv: ["jest-fetch-mock"],
+  // setupFilesAfterEnv: ["jest-fetch-mock"],
 };
